67bf6d93acda1b7340c4f529908624caf571caab
Malek <malekbr@gmail.com>
2023-08-09 22:37:44 -0400

    Clean house

--- pack.sh
+++ pack.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p package/example
-cp _build/default/example/{index.html,test.bc.js} package/example/
+cp -f _build/default/example/*.png package/example/
+cp -f _build/default/example/{index.html,test.bc.js} package/example/
 mkdir -p package/lib
-cp _build/default/lib/raylib.js package/lib/
+cp -f _build/default/lib/raylib.js package/lib/
